1adbe8e7e8362a6b73cd7785fd655010f822dff5
김민재 <lake041@korea.ac.kr>
2023-09-02 16:54:50 +0900

    Feat: 로그인 및 회원가입 기능 구현 (#3)

--- app/(main)/(routes)/page.tsx
+++ app/(main)/(routes)/page.tsx
@@ -1,7 +1,11 @@
+import { UserButton } from "@clerk/nextjs";
+
 export default function Home() {
   return (
     <div>
-      <p className="text-3xl font-bold text-indigo-500">This is a protected route.</p>
+      <p className="text-3xl font-bold text-indigo-500">
+        <UserButton afterSignOutUrl="/" />
+      </p>
     </div>
   );
 }
