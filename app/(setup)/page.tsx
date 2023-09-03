315e25a6c2a45ef9cee539430a174330886ca3de
김민재 <lake041@korea.ac.kr>
2023-09-03 19:29:53 +0900

    Feat: 서버 생성 모달 UI 및 기능 구현 (#6)
    * Style: initial-modal-ui 생성
    
    * Feat: uploadthing 이미지 업로드 구현
    
    * Feat: server 생성 구현

--- app/(setup)/page.tsx
+++ app/(setup)/page.tsx
@@ -2,6 +2,8 @@ import { redirect } from "next/navigation";
 
 import { initialProfile } from "@/lib/initial-profile";
 import { db } from "@/lib/db";
+import { InitialModal } from "@/components/modals/initial-modal";
+import { ModeToggle } from "@/components/mode-toggle";
 
 const SetupPage = async () => {
   const profile = await initialProfile();
@@ -20,7 +22,7 @@ const SetupPage = async () => {
     return redirect(`/servers/${server.id}`);
   }
 
-  return <div>Create a Server</div>;
+  return <InitialModal />;
 };
 
 export default SetupPage;
