6cd76ace1dc26e7c63d69088274f8cd390a6d18e
김민재 <lake041@korea.ac.kr>
2023-09-03 22:46:08 +0900

    Feat: 네비게이션 사이드바 구현 (#7)
    * Style: 사이드바 네비게이션 UI
    
    * Feat: 사이드바 서버 선택 기능 구현

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,8 @@ import "./globals.css";
 import type { Metadata } from "next";
 import { Open_Sans } from "next/font/google";
 import { ClerkProvider } from "@clerk/nextjs";
+
+import { ModalProvider } from "@/components/providers/modal-provider";
 import { ThemeProvider } from "@/components/providers/theme-provider";
 import { cn } from "@/lib/utils";
 
@@ -18,6 +20,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
       <html lang="en" suppressHydrationWarning>
         <body className={cn(font.className, "bg-white dark:bg-[#313338]")}>
           <ThemeProvider attribute="class" defaultTheme="dark" enableSystem={false} storageKey="discord-theme">
+            <ModalProvider />
             {children}
           </ThemeProvider>
         </body>
