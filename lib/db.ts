d9551ec2366e11f55ba091c3d5568f5b754ea574
lake041 <lake041@korea.ac.kr>
2023-09-03 16:25:01 +0900

    Style: initial-modal-ui 생성

--- lib/db.ts
+++ lib/db.ts
@@ -4,6 +4,8 @@ declare global {
   var prisma: PrismaClient | undefined;
 }
 
+// 배포 단계에서는 new PrismaClient()만 있어도 되지만,
+// 개발 단계에서는 Hot Reload 할 때마다 Client가 생성된다.
 export const db = globalThis.prisma || new PrismaClient();
 
 if (process.env.NODE_ENV !== "production") globalThis.prisma = db;
