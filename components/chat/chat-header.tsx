6d2a3ccefb6ff2cde3275f4c2560fbdae006e4ae
lake041 <lake041@korea.ac.kr>
2023-09-18 18:46:00 +0900

    Feat: Audio/Video 채팅 기능 구현

--- components/chat/chat-header.tsx
+++ components/chat/chat-header.tsx
@@ -4,6 +4,8 @@ import { MobileToggle } from "@/components/mobile-toggle";
 import { UserAvatar } from "@/components/user-avatar";
 import { SocketIndicator } from "@/components/socket-indicator";
 
+import { ChatVideoButton } from "./chat-video-button";
+
 interface ChatHeaderProps {
   serverId: string;
   name: string;
@@ -19,6 +21,7 @@ export const ChatHeader = ({ serverId, name, type, imageUrl }: ChatHeaderProps) 
       {type === "conversation" && <UserAvatar src={imageUrl} className="h-8 w-8 md:h-8 md:w-8 mr-2" />}
       <p className="font-semibold text-md text-black dark:text-white">{name}</p>
       <div className="ml-auto flex items-center">
+        {type === "conversation" && <ChatVideoButton />}
         <SocketIndicator />
       </div>
     </div>
