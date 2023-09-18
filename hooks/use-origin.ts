bc4856073e45d37972db972f69ce61f96f9ce421
lake041 <lake041@korea.ac.kr>
2023-09-18 18:02:19 +0900

    Feat: 개인 DM 기능 구현

--- hooks/use-origin.ts
+++ hooks/use-origin.ts
@@ -1,3 +1,6 @@
+// SSR 환경에서 초기 렌더링 시점에 클라이언트 측 전용 객체인 window 객체에 접근하려고 하면 에러가 발생한다.
+// 따라서 mounted 상태 값을 통해 실제 클라이언트 환경에서만 window.location.origin 값에 접근하도록 한다.
+
 import { useEffect, useState } from "react";
 
 export const useOrigin = () => {
